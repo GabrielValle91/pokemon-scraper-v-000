9eb7efc6ca01aae675f98ccaf26105e64173bdd0
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 02:17:52 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -11,7 +11,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    record = db.execute("SELECT pokemon.id FROM pokemon WHERE pokemon.id = #{id};")
+    record = db.execute("SELECT pokemon.id FROM pokemon WHERE pokemon.id = #{id};").to_i
     #binding.pry
     @@all[record - 1]
   end
